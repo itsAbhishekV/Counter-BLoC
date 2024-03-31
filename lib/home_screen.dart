2b397adc54a59b2a762976602e785f458ae152f4
Abhishek Verma <abhishekverma7t@gmail.com>
2024-03-31 13:04:12 +0530

    bloc functionality added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_counter_app/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,36 +10,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int counter = 0;
+  final counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter App with BLoC'),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Counter',
-            style: TextStyle(fontSize: 24),
-          ),
-          Text(
-            '$counter',
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+    return BlocBuilder<CounterCubit, int>(
+        bloc: counterCubit,
+        builder: (context, counter) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Counter App with BLoC'),
+            ),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Counter',
+                  style: TextStyle(fontSize: 24),
+                ),
+                Text(
+                  '$counter',
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            )),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                counterCubit.increment();
+              },
+              child: const Icon(Icons.add),
+            ),
+          );
+        });
   }
 }
